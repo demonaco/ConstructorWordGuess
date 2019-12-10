4108097b7720e15a9069952ac3d58da2aba8b663
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-10 00:34:55 -0800

    added variable for guesses, created beginning logic for checking against correct letter.

--- Letter.js
+++ Letter.js
@@ -7,16 +7,35 @@ var correct =
 
 var guess = process.argv[3]
 
+var guesses = 10
+
 var characterGuess = function Letter(guess, boolean, answer, character) {
-this.guess = "",
-this.boolean = true,
-this.answer = function(){
-return this.guess
+    this.guess = "",
+        this.boolean = true,
+        this.answer = function () {
+            return this.guess
+        }
+    this.character = function () {
+        if (this.guess === correct)
+            this.boolean = true;
+    } else {
+        this.boolean = false;
+    }
 }
-this.character = function(){
-    if (this.guess === correct)
-    this.boolean = true;
-} else {
-    this.boolean = false;
-} 
+
+function guessSystem() {
+    inquirer.prompt([
+        {
+            message: "What is your letter guess?"
+        }
+
+    ]).then(function (guess) {
+        for (var i = 0; i < array.length; i++)
+            if (guess === array[i]) {
+                console.log(guess.printInfo)
+            } else {
+                console.log("Sorry that was incorrect")
+            }
+        guessSystem();
+    })
 }
