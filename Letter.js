2c495f27037fec4555e6989bb3074f2d504c9353
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-09 18:24:07 -0800

    stated coding constructor function for letter guess. global variables set up along with NPM required files/modules.

---
+++ Letter.js
@@ -0,0 +1,22 @@
+
+var inquirer = require('inquirer');
+
+var prompt = require('prompt');
+
+var correct = 
+
+var guess = process.argv[3]
+
+var characterGuess = function Letter(guess, boolean, answer, character) {
+this.guess = "",
+this.boolean = true,
+this.answer = function(){
+return this.guess
+}
+this.character = function(){
+    if (this.guess === correct)
+    this.boolean = true;
+} else {
+    this.boolean = false;
+} 
+}
