ea222ba6a4344a292669d4174fcb564cd28f459c
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-10 23:18:05 -0800

    added constructor to hold words.

--- Letter.js
+++ Letter.js
@@ -3,47 +3,59 @@ var inquirer = require('inquirer');
 
 var prompt = require('prompt');
 
-var correct = 
-
-
 var guesses = 10
 
-function startover(characterguess){
-};
 
-var characterGuess = function Letter(guess, letter, character, answer) {
+var characterGuess = function Letter(guess, letter, character, display) {
+    console.log("hello welcome");
     this.guess = process.argv[3],
         this.letter = false,
         this.character = function () {
             for (var i = 0; i < array.length; i++)
-            if (this.guess === array[i]) {
-                this.letter = true;
-                console.log(this.letter);
-            return (this.guess);
-        } else {
-            this.letter = false;
-            guesses--;
-            if (guesses === 0) {
-                console.log("Sorry you lost the game!")
-                startover();
-            } 
+                if (this.guess === array[i]) {
+                    this.letter = true;
+                    console.log(this.letter);
+                    console.log(this.guess);
+                } else {
+                    this.letter = false;
+                    guesses--;
+                    if (guesses === 0) {
+                        console.log("Sorry you lost the game!")
+                        startover();
+                    }
+                }
+        }
+    this.display = function () {
+        if (this.guess === array[i]) {
+            this.letter = true;
+            return this.guess
         }
     }
-     }
-     startover();
+}
 
-    letter.prototype.toString = function() {
-        var correct = "Correct " + this.guess;
+function startover(characterGuess) {
+};
 
-        if this.letter = 
+
+module.exports = characterGuess;
+// below is extra code to use for later sections
+
+
+
+
+
+letter.prototype.toString = function () {
+    var correct = "Correct " + this.guess;
+
+    if this.letter = 
     }
 
-    this.character = function () {
-        if (this.guess === correct)
-            this.boolean = true;
-    } else {
-        this.boolean = false;
-    }
+this.character = function () {
+    if (this.guess === correct)
+        this.boolean = true;
+} else {
+    this.boolean = false;
+}
 }
 
 function guessSystem() {
--- Word.js
+++ Word.js
@@ -1,3 +1,30 @@
 var letters = require('./Letter.js');
 
-var newLetter = []
+ 
+
+function Word(newLetter, words, argument) {
+    var newLetter =
+        [{
+            jurassicPark = new Letters(j, u, r a, s, i, c, p, k),
+            familyGuy = new Letters(f, a, m, i, l, y, g, u),
+            beatles = new Letters(t, h, e, b, a, l, s),
+            toyStory = new Letters(t, o, y, s, t, r)
+        }]
+},
+
+this.words = 
+{
+    this.example1 = "jurassic park",
+    this.example2 = "family guy",
+    this.example3 = "the beatles",
+    this.example4 = "toy story"
+} Letter();,
+
+    this.argument = function(){
+        var userArg = process.argv[3];
+        Letter(userArg);
+    }
+
+
+
+
