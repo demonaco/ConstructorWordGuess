d1d9265a0d5b051c100106f185c25ccbff6a6ff0
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-10 23:29:20 -0800

    added Word constructor as well as logic in index.html.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 var word = require("./Word.js")
+
+
+function randomWord() {
+    var ranArray = ["jurassic park", "toy story", "the beatles", "family guy"]
+    for (i = 0; i < ranArray.length; i++);
+    word(array[i]);
+
+    function ask(){
+    inquirer.prompt([
+        {
+            message: "What is your letter guess?"
+        }
+
+    ]).then(function (guess) {
+        var guess = process.argv[3];
+        for (var i = 0; i < Newletter.length; i++)
+            if (guess === array[i]) {
+                console.log(guess.printInfo)
+            } else {
+                console.log("Sorry that was incorrect")
+            }
+        
+    })
+}
+ask();
+}
